62029486d7fc7b4782ba5c8520e499689b8be4f3
baldx <mr.ahmad.m2006@gmail.com>
2023-10-16 20:17:19 +0200

    add imports & change function names

--- tests/analyzeArray.test.js
+++ tests/analyzeArray.test.js
@@ -1,15 +1,15 @@
-import averageValue from "../scripts/analyzeArray";
+import {averageValue, minValue} from "../scripts/analyzeArray";
 
 it('return average value', () => {
     expect(averageValue([1, 8, 3, 4, 2, 6])).toBe(4);
 })
 
 it('Min value', () => {
-    expect(min([1,8,3,4,2,6])).toBe(1);
+    expect(minValue([1,8,3,4,2,6])).toBe(1);
 })
 
 it('Max value', () => {
-    expect(max([1,8,3,4,2,6])).toBe(8);
+    expect(maxValue([1,8,3,4,2,6])).toBe(8);
 })
 
 it('Array length', () => {
